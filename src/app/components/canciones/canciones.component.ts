efb406a7ccfd57a2fefd9c251eda0488914b1aa9
henrynar <henrynarvaezchavez851@gmail.com>
2024-05-22 12:01:50 -0500

    initial

--- src/app/components/canciones/canciones.component.ts
+++ src/app/components/canciones/canciones.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,8 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./canciones.component.css']
 })
 export class CancionesComponent {
+  @ViewChild('audioPlayer') audioPlayer: any;
+  audioText: { word: string, startTime: number, endTime: number }[] = [
+    { word: 'Yo conozco', startTime: 16, endTime: 17 },
+    { word: 'conozco', startTime: 0.16, endTime: 2.5 },
+    // Agrega más palabras con sus tiempos de inicio y finalización
+  ];
+  currentWordIndex: number = -1;
 
+  highlightWord(audioPlayer: HTMLAudioElement) {
+    const currentTime = audioPlayer.currentTime;
 
+    // Busca la palabra actual basada en el tiempo actual de reproducción del audio
+    for (let i = 0; i < this.audioText.length; i++) {
+      if (currentTime >= this.audioText[i].startTime && currentTime <= this.audioText[i].endTime) {
+        this.currentWordIndex = i;
+        break;
+      }
+    }
+  }
 
   items = [
 
